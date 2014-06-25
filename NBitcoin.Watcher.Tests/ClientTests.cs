9cf46c48ee76749a5e08b58fab8a1305056e75fa
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-06-25 18:45:58 +0200

    missing file

---
+++ NBitcoin.Watcher.Tests/ClientTests.cs
@@ -0,0 +1,68 @@
+﻿using NBitcoin.Watcher.Client;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace NBitcoin.Watcher.Tests
+{
+	[TestFixture]
+	public class ClientTests
+	{
+		[Test]
+		public async Task CanPing()
+		{
+			using(var tester = CreateTester())
+			{
+				await tester.Client.Ping();
+				await tester.Client.Ping();
+			}
+		}
+
+
+		[Test]
+		public void CanParseAndSerializeWatch()
+		{
+			var watch = new PubKeyHashWatch();
+			var watch2 = Watch.Parse(watch.ToString());
+			var watch3 = Watch.Parse<PubKeyHashWatch>(watch.ToString());
+			Assert.True(watch2 is PubKeyHashWatch);
+			Assert.AreEqual(watch.ToString(), watch2.ToString());
+			Assert.AreEqual(watch.ToString(), watch3.ToString());
+		}
+		[Test]
+		public async Task CanCRUDWatches()
+		{
+			using(var tester = CreateTester())
+			{
+				var k1 = new Key().PubKey.GetAddress(Network.Main).ToString();
+				var k2 = new Key().PubKey.GetAddress(Network.Main).ToString();
+				await tester.Client.AddWatches(new[] { k1, k2 }.Select(k => new PubKeyHashWatch(k)));
+
+				var actualWatches = (await tester.Client.ListWatches()).OfType<PubKeyHashWatch>().ToArray();
+
+				var w1 = actualWatches.FirstOrDefault(o => o.Address == k1);
+				Assert.True(w1 != null);
+				Assert.True(actualWatches.Any(o => o.Address == k2));
+
+				await tester.Client.DeleteWatches(new string[] { w1.Name });
+
+				actualWatches = (await tester.Client.ListWatches()).OfType<PubKeyHashWatch>().ToArray();
+
+				Assert.False(actualWatches.Any(o => o.Address == k1));
+				Assert.True(actualWatches.Any(o => o.Address == k2));
+			}
+		}
+
+		private WatcherTester CreateTester([CallerMemberName]string folder = null)
+		{
+			return new WatcherTester(folder);
+		}
+	}
+}
---
+++ NBitcoin.Watcher.Tests/WatcherTester.cs
@@ -0,0 +1,57 @@
+﻿using NBitcoin.Watcher.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+
+namespace NBitcoin.Watcher.Tests
+{
+	public class WatcherTester : IDisposable
+	{
+		string _Folder;
+
+
+		public WatcherTester(string folder)
+		{
+			TestUtils.EnsureNew(folder);
+			_Folder = folder;
+
+			WatcherOptions watcher = new WatcherOptions();
+			watcher.Port = 80;
+			watcher.Path = "WatcherTests/" + folder;
+			watcher.WatchDirectory = folder;
+			_Client = new WatcherClient(watcher.CreateBaseAddress());
+			_Server = watcher.OpenSelfHost().Result;
+		}
+
+		private readonly HttpSelfHostServer _Server;
+		public HttpSelfHostServer Server
+		{
+			get
+			{
+				return _Server;
+			}
+		}
+
+		private readonly WatcherClient _Client;
+		public WatcherClient Client
+		{
+			get
+			{
+				return _Client;
+			}
+		}
+
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			_Server.Dispose();
+		}
+
+		#endregion
+	}
+}
